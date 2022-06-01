adea3d746e58fa60da51364ba8244737d1b020cd
unknown <fm.bitcoding@gmail.com>
2022-06-01 15:12:33 +0530

    hello

--- ecom/main/views.py
+++ ecom/main/views.py
@@ -1,3 +1,4 @@
+from turtle import title
 from unicodedata import category
 from django.shortcuts import render,redirect
 from django.http import JsonResponse,HttpResponse
@@ -7,50 +8,52 @@ from django.template.loader import render_to_string
 
 # Home Page
 def home(request):
-	banners = Banner.objects.all().order_by('-id')
-	data = Product.objects.filter(is_featured=True).order_by('-id')
+	banners=Banner.objects.all().order_by('-id')
+	data=Product.objects.filter(is_featured=True).order_by('-id')
 	return render(request,'index.html',{'data':data,'banners':banners})
 
 # Category
 def category_list(request):
-	data = Category.objects.all().order_by('-id')
-	return render(request,'category_list.html',{'data':data})
+    data=Category.objects.all().order_by('-id')
+    return render(request,'category_list.html',{'data':data})
 
 # Brand
 def brand_list(request):
-	data = Brand.objects.all().order_by('-id')
-	return render(request,'brand_list.html',{'data':data})
+    data=Brand.objects.all().order_by('-id')
+    return render(request,'brand_list.html',{'data':data})
 
 # Product List
 def product_list(request):
 	total_data=Product.objects.count()
-	data = Product.objects.all().order_by('-id')
+	data=Product.objects.all().order_by('-id')[:3]
 	min_price=ProductAttribute.objects.aggregate(Min('price'))
 	max_price=ProductAttribute.objects.aggregate(Max('price'))
-	return render(request,'product_list.html',{'data':data,'total_data':total_data,'min_price':min_price,'max_price':max_price,})
+	return render(request,'product_list.html',{'data':data,'total_data':total_data,'min_price':min_price,'max_price':max_price})
 
 # Product List According to Category
 def category_product_list(request,cat_id):
-	category = Category.objects.get(id=cat_id)
-	data = Product.objects.filter(category=category).order_by('-id')
+	category=Category.objects.get(id=cat_id)
+	data=Product.objects.filter(category=category).order_by('-id')
 	return render(request,'category_product_list.html',{'data':data})
 
 # Product List According to Brand
 def brand_product_list(request,brand_id):
-	brand = Brand.objects.get(id=brand_id)
-	data = Product.objects.filter(brand=brand).order_by('-id')	
+	brand=Brand.objects.get(id=brand_id)
+	data=Product.objects.filter(brand=brand).order_by('-id')
 	return render(request,'category_product_list.html',{'data':data})
 
 # Product Detail
 def product_detail(request,slug,id):
-	product = Product.objects.get(id=id)
-	related_products = Product.objects.filter(category=product.category).exclude(id=id)[:4]
-	return render(request, 'product_detail.html',{'data':product,'related':related_products})
+	product=Product.objects.get(id=id)
+	related_products=Product.objects.filter(category=product.category).exclude(id=id)[:4]
+	colors=ProductAttribute.objects.filter(product=product).values('color__id','color__title','color__color_code').distinct()
+	sizes=ProductAttribute.objects.filter(product=product).values('size__id','size__title','price','color__id').distinct()
+	return render(request, 'product_detail.html',{'data':product,'related':related_products,'colors':colors,'sizes':sizes})
 
 # Search
 def search(request):
-	q = request.GET['q']
-	data = Product.objects.filter(title__icontains=q).order_by('-id')
+	q=request.GET['q']
+	data=Product.objects.filter(title__icontains=q).order_by('-id')
 	return render(request,'search.html',{'data':data})
 
 # Filter Data
@@ -77,11 +80,34 @@ def filter_data(request):
 
 # Load More
 def load_more_data(request):
-	return render(request,'ajax/product-list.html')
+	offset = int(request.GET['offset'])
+	limit = int(request.GET['limit'])
+	data = Product.objects.all().order_by('-id')[offset:offset+limit]
+	t = render_to_string('ajax/product-list.html',{'data':data})
+	return JsonResponse({'data':t})
 
 # Add to cart
 def add_to_cart(request):
-	pass
+	cart_p={}
+	cart_p[str(request.GET['id'])]={
+		'image':request.GET['image'],
+		'title':request.GET['title'],
+		'qty':request.GET['qty'],
+		'price':request.GET['price'],
+	}
+	if 'cartdata' in request.session:
+		if str(request.GET['id']) in request.session['cartdata']:
+			cart_data=request.session['cartdata']
+			cart_data[str(request.GET['id'])]['qty']=int(cart_p[str(request.GET['id'])]['qty'])
+			cart_data.update(cart_data)
+			request.session['cartdata']=cart_data
+		else:
+			cart_data=request.session['cartdata']
+			cart_data.update(cart_p)
+			request.session['cartdata']=cart_data
+	else:
+		request.session['cartdata']=cart_p
+	return JsonResponse({'data':request.session['cartdata'],'totalitems':len(request.session['cartdata'])})
 # Cart List Page
 def cart_list(request):
 	return render(request, 'cart.html')
